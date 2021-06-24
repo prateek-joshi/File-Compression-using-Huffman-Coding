68a3886c8da6e519e43a36715c5f6dbd7c7412a3
Prateek Joshi <pjoshi22900@gmail.com>
2021-06-24 17:53:53 +0530

    Defined Huffman Tree Creation function and minHeap structure

--- utils/minHeap.h
+++ utils/minHeap.h
@@ -1,3 +1,18 @@
-int add(int p, int q){
-    return p+q;
-}
+
+struct minHeapNode{
+    char data;
+    unsigned int freq;
+    minHeapNode *left, *right;
+
+    minHeapNode(char data, unsigned int freq){
+        left = right = nullptr;
+        this->data = data;
+        this->freq = freq;
+    }
+};
+
+struct compare {
+    bool operator()(minHeapNode* l, minHeapNode* r){
+        return (l->freq > r->freq);
+    }
+};
