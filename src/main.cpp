486ce68581b4d30740b5fd17fe7f19ed274fcc20
Prateek Joshi <pjoshi22900@gmail.com>
2021-06-26 20:39:11 +0530

    Filename can be given as CL argument

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,15 @@
 #include<iostream>
 using namespace std;
 
-// Creates a minHeap for generating Huffman codes
+int main(int argc, char *argv[]){
+    // check if filename is provided.
+    if(argc==1)
+        cout<< "Filename not provided.\n";
+    else{
+        minHeapNode *root = HuffmanCodes(argv[1]);
+        generateCodes(root, (char*)"");
 
-int main(){
-    // TODO: write a function to calculate the frequency of all characters in a file.
-
-    char data[] = {'a','f','d','s','e'}; //contains list of characters
-    int freq[] = {10,2,3,5,6}; //frequency of each character
-
-    int n = sizeof(data)/sizeof(data[0]);
-
-    minHeapNode *root = HuffmanCodes(data, freq, n);
-    generateCodes(root, (char*)"");
-    
-    cout<<endl;
+        cout<<endl;
+    }
     return 0;
 }
