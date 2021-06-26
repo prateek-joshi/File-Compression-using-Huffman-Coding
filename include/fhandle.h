a2be77118c5ce68453147bca2c6543d4fdbca2a7
Prateek Joshi <pjoshi22900@gmail.com>
2021-06-26 22:27:18 +0530

    Fixed bug #1: Keys assigned for random letters not in file

--- include/fhandle.h
+++ include/fhandle.h
@@ -18,26 +18,24 @@ unordered_map<char, int> getFrequencyFromFile(const char *filename){
     fstream file;
     unordered_map<char, int> charFreq;
     // unordered_map<char, int>:: iterator it;
-    char *line = (char*)malloc(LINE_SIZE*sizeof(char));
+    string line;
 
     opener(file, filename, ios::in);
 
     while(!file.eof()){
-        file.getline(line,LINE_SIZE);
+        getline(file,line);
         // add characterwise frequency to map
-        for(int i=0; i<LINE_SIZE; i++){
-            // myrecipe.insert (std::make_pair<std::string,double>("eggs",6.0));
-            if(charFreq.find(line[i]) == charFreq.end()){
-                // not found, insert new pair
-                charFreq.insert({line[i],1});
-            }
-            else{
-                // increment existing frequency term
+        for(int i=0; i<line.size(); i++){
+            if(charFreq.find(line[i])==charFreq.end())
+                charFreq[line[i]] = 1;
+            else
                 charFreq[line[i]]++;
-            }
         }
     }
 
+    for(auto it: charFreq)
+        cout<<it.first<<' '<<it.second<<endl;
+    cout<<endl;
     file.close();
     return charFreq;
 }
