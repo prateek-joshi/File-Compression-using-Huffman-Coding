f77c160b87127bcd10da7ba18a1ff05af081a6cf
Prateek Joshi <pjoshi22900@gmail.com>
2021-06-26 20:34:10 +0530

    Bug in getFrequencyFromFile function

--- include/fhandle.h
+++ include/fhandle.h
@@ -1,21 +1,23 @@
+#pragma once
 #define LINE_SIZE 100
 #include<fstream>
 #include<cstring>
 #include<unordered_map>
+#include<iostream>
 using namespace std;
 
 void opener(fstream & file, const char *filename, ios_base::openmode mode){
-    try{
-        file.open(filename, mode);
-    } catch(exception & e){
-        throw strcat((char*)"Could not open the file: ",filename);
+    file.open(filename, mode);
+    if(!file){
+        cout<< "Could not open the file: "<<filename<<endl;
+        exit(1);
     }
 }
 
-unordered_map<char, int> & getFrequencyFromFile(const char *filename){
+unordered_map<char, int> getFrequencyFromFile(const char *filename){
     fstream file;
     unordered_map<char, int> charFreq;
-    unordered_map<char, int>:: iterator it;
+    // unordered_map<char, int>:: iterator it;
     char *line = (char*)malloc(LINE_SIZE*sizeof(char));
 
     opener(file, filename, ios::in);
@@ -24,7 +26,15 @@ unordered_map<char, int> & getFrequencyFromFile(const char *filename){
         file.getline(line,LINE_SIZE);
         // add characterwise frequency to map
         for(int i=0; i<LINE_SIZE; i++){
-            charFreq[line[i]]++;
+            // myrecipe.insert (std::make_pair<std::string,double>("eggs",6.0));
+            if(charFreq.find(line[i]) == charFreq.end()){
+                // not found, insert new pair
+                charFreq.insert({line[i],1});
+            }
+            else{
+                // increment existing frequency term
+                charFreq[line[i]]++;
+            }
         }
     }
 
