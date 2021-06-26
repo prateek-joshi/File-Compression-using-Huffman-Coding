603267ffd8a20a7f35cab0825ba3bfd901001340
Prateek Joshi <pjoshi22900@gmail.com>
2021-06-26 17:57:14 +0530

    Add function to get frequency of all characters in file

--- include/fhandle.h
+++ include/fhandle.h
@@ -1,11 +1,34 @@
+#define LINE_SIZE 100
 #include<fstream>
 #include<cstring>
+#include<unordered_map>
+//for now
+#include<iostream>
 using namespace std;
 
-void opener(fstream & file, char *filename, ios_base::openmode mode){
+void opener(fstream & file, const char *filename, ios_base::openmode mode){
     try{
         file.open(filename, mode);
     } catch(exception & e){
         throw strcat((char*)"Could not open the file: ",filename);
     }
+}
+
+unordered_map<char, int> & getFrequencyFromFile(const char *filename){
+    fstream file;
+    unordered_map<char, int> charFreq;
+    unordered_map<char, int>:: iterator it;
+    char *line = (char*)malloc(LINE_SIZE*sizeof(char));
+
+    opener(file, filename, ios::in);
+
+    while(!file.eof()){
+        file.getline(line,LINE_SIZE);
+        for(int i=0; i<LINE_SIZE; i++){
+            charFreq[line[i]]++;
+        }
+    }
+
+    file.close();
+    return charFreq;
 }
