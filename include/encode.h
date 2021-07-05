ab64283e65d7c1afc75fe60797fb9f279cad34c5
Prateek Joshi <pjoshi22900@gmail.com>
2021-07-05 22:51:34 +0530

    Add function to convert file into binary string

--- include/encode.h
+++ include/encode.h
@@ -4,10 +4,10 @@
 #include<iostream> //temporary
 using namespace std;
 
-unordered_map<string,long long> unpackCodesFromFile(string codefile){
+unordered_map<string,string> unpackCodesFromFile(string codefile){
     fstream file;
     opener(file,codefile.c_str(),ios::in);
-    unordered_map<string,long long> codes;
+    unordered_map<string,string> codes;
     string encoded_text;
 
     while(!file.eof()){
@@ -15,11 +15,39 @@ unordered_map<string,long long> unpackCodesFromFile(string codefile){
         char *end;
 
         getline(file, key, '|');
-        getline(file,code,'\n');
-
-        codes[key] = strtoll(code.c_str(),&end,2);
+        getline(file,codes[key],'\n');
     }
 
+    file.close();
     return codes;
 }
 
+// TODO
+long long encodeText(unordered_map<string, long long> codes, string line){
+    long long compressedText = 0;
+
+    return compressedText;
+}
+
+void compressFile(string codefile, string file) {
+    unordered_map<string, string> codes = unpackCodesFromFile(codefile);
+    string encodedString = "", line = "";
+    
+    // temporary
+    // for(auto i: codes)
+    //     cout<<i.first<<' '<<i.second<<endl;
+
+    fstream datafile;
+    opener(datafile,file.c_str(),ios::in);
+
+    while(!datafile.eof()){
+        getline(datafile, line);
+        for(int i: line){
+            string key(1, i);
+            encodedString += codes[key];
+        }
+        encodedString += codes["\\n"];
+    }
+    cout<<encodedString<<endl;
+    datafile.close();
+}
